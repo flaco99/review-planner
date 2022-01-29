c5455f9cc77197de2ded3a44f6177c9dad1f8ae2
naomi <naomi@neopatent.com>
2022-01-29 15:50:00 -0800

    print exception stack trace when errors occur

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import traceback
 
 app = Flask(__name__)
 
@@ -40,25 +41,28 @@ function onSignIn(googleUser) {
 
 @app.route('/dumdum/<idtoken>')
 def about(idtoken):
-    creds = Credentials(None, id_token=idtoken)
-    service = build('calendar', 'v3', credentials=creds)
+    try:
+        creds = Credentials(None, id_token=idtoken)
+        service = build('calendar', 'v3', credentials=creds)
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
-    if not events:
-        return 'No upcoming events found.'
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+        if not events:
+            return 'No upcoming events found.'
 
-    result = ""
-    # Prints the start and name of the next 10 events
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        result += f"{start} {event['summary']}"
-    return result
+        result = ""
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            result += f"{start} {event['summary']}"
+        return result
+    except Exception as e:
+        return str(traceback.format_exc())
 
 
 @app.route('/portfolio')
