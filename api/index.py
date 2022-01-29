5ade2d1ddac79884296301fb3e34f9d808dd2787
nTZW <74113571+flaco99@users.noreply.github.com>
2022-01-29 14:01:59 -0800

    stick to the provided example

--- api/index.py
+++ api/index.py
@@ -1,14 +1,7 @@
 from flask import Flask, Response
 app = Flask(__name__)
 
-#@app.route('/<path:path>')
 @app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def catch_all(path):
-    message = cow.Cowacter().milk('Hello from Python from a Serverless Function!  yesssssssssss')
-    return Response("<pre>/%s</pre>" % (message), mimetype="text/html")
-    #return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
-    
-
-@app.route('/hello')
-def hello(path):
-    return Response("<pre>/hello something else</pre>", mimetype="text/html")
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
