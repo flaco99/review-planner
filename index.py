0e7d21b0a1bba6a019d78ed75884fdd9ef7d219b
nTZW <74113571+flaco99@users.noreply.github.com>
2022-01-29 14:22:36 -0800

    add js debug code after auth

--- index.py
+++ index.py
@@ -12,6 +12,15 @@ def home():
     <title>just a page</title>
     <script src="https://apis.google.com/js/platform.js" async defer></script>
     <meta name="google-signin-client_id" content="769017929605-rpovnc5j01547ktb1er5fc0qqcd6fcer.apps.googleusercontent.com">
+    <script>
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}    
+    </script>
     </head>
     <body>
     <p>Hello, I'm in html</p>
