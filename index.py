e2e474b9081f5103595476a382f462895e216872
nTZW <74113571+flaco99@users.noreply.github.com>
2022-01-29 15:22:47 -0800

    use calendar api

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 from flask import Flask
-
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 app = Flask(__name__)
 
@@ -34,9 +38,28 @@ function onSignIn(googleUser) {
     return html
 
 
-@app.route('/dumdum/<tokenid>')
-def about(tokenid):
-    return f"dumdum {tokenid}"
+@app.route('/dumdum/<idtoken>')
+def about(idtoken):
+    creds = Credentials(id_token=idtoken)
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    if not events:
+            return 'No upcoming events found.'
+            return
+
+    result = ""
+    # Prints the start and name of the next 10 events
+    for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            result += f"{start} {event['summary']}"
+    return result
 
 
 @app.route('/portfolio')
