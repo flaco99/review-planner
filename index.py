6bcda96bf4c14ce5031b3811d451b50bb373eec7
naomi <naomi@neopatent.com>
2022-02-06 12:58:42 -0800

    make sure it still works without the new event

--- index.py
+++ index.py
@@ -30,10 +30,6 @@ def home():
 
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
 
-    # calendar.insert(calendarId='primary', start=now, summary="something", description='some description',
-    #                conferenceDataVersion=None, maxAttendees=None, sendNotifications=None, sendUpdates=None, supportsAttachments=None)
-
-
     print('Getting the upcoming 10 events')
     events_result = calendar.events().list(calendarId='primary', timeMin=now,
                                           maxResults=10, singleEvents=True,
