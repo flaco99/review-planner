ab2eb5f08b26486a835ae711733281ff0e23aee5
nTZW <74113571+flaco99@users.noreply.github.com>
2022-02-19 09:57:46 -0800

    make the intervals between the events adjustable

--- index.py
+++ index.py
@@ -34,17 +34,21 @@ def create():
 
 
     # set review events
+
+    # duration of event shrinks when days increase
+
+    # also must review 10 minutes immediately after class/ when you first learn it
+
     day0 = datetime.datetime.today() - datetime.timedelta(days=1)
-    day1 = (day0 + datetime.timedelta(days=1)).isoformat() + 'Z'
-    day2 = (day0 + datetime.timedelta(days=3)).isoformat() + 'Z'
-    day3 = (day0 + datetime.timedelta(days=6)).isoformat() + 'Z'
-    day4 = (day0 + datetime.timedelta(days=13)).isoformat() + 'Z'
-    day5 = (day0 + datetime.timedelta(days=27)).isoformat() + 'Z'
-    day6 = (day0 + datetime.timedelta(days=55)).isoformat() + 'Z'
-    day7 = (day0 + datetime.timedelta(days=83)).isoformat() + 'Z'
-    day8 = (day0 + datetime.timedelta(days=111)).isoformat() + 'Z'
-    day9 = (day0 + datetime.timedelta(days=139)).isoformat() + 'Z'
-    for day in [day1, day2, day3, day4, day5, day6, day7, day8, day9]:
+
+    maxdays = 60
+    daysplus = 1
+    intervalFactor = 3
+    duration = 10
+    while daysplus<maxdays:
+        daysplus=daysplus*intervalFactor
+        duration=duration/2
+        day = (day0 + datetime.timedelta(days=daysplus)).isoformat() + 'Z'
         event = {
             'summary': eventname,
             'start': {
@@ -54,6 +58,26 @@ def create():
                 'dateTime': day,
             }
         }
+    #
+    # day1 = (day0 + datetime.timedelta(days=1)).isoformat() + 'Z'
+    # day2 = (day0 + datetime.timedelta(days=3)).isoformat() + 'Z'
+    # day3 = (day0 + datetime.timedelta(days=6)).isoformat() + 'Z'
+    # day4 = (day0 + datetime.timedelta(days=13)).isoformat() + 'Z'
+    # day5 = (day0 + datetime.timedelta(days=27)).isoformat() + 'Z'
+    # day6 = (day0 + datetime.timedelta(days=55)).isoformat() + 'Z'
+    # day7 = (day0 + datetime.timedelta(days=83)).isoformat() + 'Z'
+    # day8 = (day0 + datetime.timedelta(days=111)).isoformat() + 'Z'
+    # day9 = (day0 + datetime.timedelta(days=139)).isoformat() + 'Z'
+    # for day in [day1, day2, day3, day4, day5, day6, day7, day8, day9]:
+    #     event = {
+    #         'summary': eventname,
+    #         'start': {
+    #             'dateTime': day,
+    #         },
+    #         'end': {
+    #             'dateTime': day,
+    #         }
+    #     }
         # Call the Calendar API
         calendar.events().insert(calendarId='primary', sendNotifications=True, body=event).execute()
 
