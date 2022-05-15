0e1e9eb39c5f5d5657e46b1cc2d22d1fb8e89d62
nTZW <74113571+flaco99@users.noreply.github.com>
2022-05-15 13:23:37 -0700

    added edit.html and apply_changes_to_all in index.py. Now, user can edit and update the title of the most recently created event and it's repeated copies. Next steps: 1: be able to edit the rest of the event's attributes. 2: instead of manually making a new tagID, generate non-repeating tagIDs.

---
+++ static/edit.js
@@ -0,0 +1,4 @@
+// go to the user's google calendar and get info on the event that they're editting and fill in the info
+
+// localStorage.setItem('event_hour',document.querySelector("#eventhour").value);
+// like this but without using local storage.
